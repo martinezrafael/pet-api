fc113316d72dd9af961d0447578fd18289a080b7
rafael martinez <rafaelmartinez.contato@gmail.com>
2024-04-16 18:57:51 -0300

    add `src/app.js` and `server.js`

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/teste', (req, res)=>{
+    res.status(200).send({mensagem: 'Boas-vindas a API'})
+})
+
+module.exports = app;
