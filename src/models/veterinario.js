e168bf05c8802a4f7fddf2da9e11afd9593a1f18
rafael martinez <rafaelmartinez.contato@gmail.com>
2024-04-17 19:09:08 -0300

    add a tablename in model Veterinario

--- src/models/veterinario.js
+++ src/models/veterinario.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Veterinario extends Model {
     /**
@@ -13,26 +11,30 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Veterinario.init({
-    veterinario_nome: DataTypes.STRING,
-    veterinario_sobrenome: DataTypes.STRING,
-    veterinario_cpf: DataTypes.STRING,
-    veterinario_crmv: DataTypes.STRING,
-    veterinario_foto: DataTypes.STRING,
-    veterinario_telefone1: DataTypes.STRING,
-    veterinario_telefone2: DataTypes.STRING,
-    endereco_rua: DataTypes.STRING,
-    endereco_numero: DataTypes.INTEGER,
-    endereco_complemento: DataTypes.STRING,
-    endereco_bairro: DataTypes.STRING,
-    endereco_cidade: DataTypes.STRING,
-    endereco_estado: DataTypes.STRING,
-    endereco_cep: DataTypes.STRING,
-    veterinario_email: DataTypes.STRING,
-    veterinario_password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Veterinario',
-  });
+  Veterinario.init(
+    {
+      veterinario_nome: DataTypes.STRING,
+      veterinario_sobrenome: DataTypes.STRING,
+      veterinario_cpf: DataTypes.STRING,
+      veterinario_crmv: DataTypes.STRING,
+      veterinario_foto: DataTypes.STRING,
+      veterinario_telefone1: DataTypes.STRING,
+      veterinario_telefone2: DataTypes.STRING,
+      endereco_rua: DataTypes.STRING,
+      endereco_numero: DataTypes.INTEGER,
+      endereco_complemento: DataTypes.STRING,
+      endereco_bairro: DataTypes.STRING,
+      endereco_cidade: DataTypes.STRING,
+      endereco_estado: DataTypes.STRING,
+      endereco_cep: DataTypes.STRING,
+      veterinario_email: DataTypes.STRING,
+      veterinario_password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Veterinario",
+      tableName: "veterinarios",
+    },
+  );
   return Veterinario;
 };
