72e15a1f28005737f8a8d8a1994463b839e4a858
rafael martinez <rafaelmartinez.contato@gmail.com>
2024-04-17 16:28:30 -0300

    install `sequelize`, `pg` and `pg-store`

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./src/app.js');
 const PORT = 3000;
 
 app.listen(PORT, ()=> {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Servidor rodando na porta:${PORT}`)
 })
