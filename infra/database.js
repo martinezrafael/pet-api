6eb20c9391fbb6c461cfe52eed9e48e9a25bc86f
rafael martinez <rafaelmartinez.contato@gmail.com>
2024-04-19 19:47:11 -0300

    create migrations

--- infra/database.js
+++ infra/database.js
@@ -1,7 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-console.log(process.env.DATABASE);
-
 const sequelize = new Sequelize(
   process.env.POSTGRES_DB,
   process.env.POSTGRES_USER,
@@ -17,6 +15,23 @@ async function testConnection() {
   try {
     await sequelize.authenticate();
     console.log("Conexão estabelecida com sucesso.");
+
+    // Consulta SQL para obter todas as tabelas no banco de dados
+    const query = `
+      SELECT table_name
+      FROM information_schema.tables
+      WHERE table_schema = 'public'
+      AND table_type = 'BASE TABLE';
+    `;
+
+    // Executa a consulta SQL diretamente no banco de dados
+    const [results, metadata] = await sequelize.query(query);
+
+    // Exibe o nome das tabelas
+    console.log("Tabelas no banco de dados:");
+    results.forEach((row) => {
+      console.log(row.table_name);
+    });
   } catch (error) {
     console.error("Erro ao conectar ao banco de dados:", error);
   } finally {
