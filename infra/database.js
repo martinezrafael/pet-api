8d6037096c9fdf48bed5252175f26e5ff419b2fc
rafael martinez <rafaelmartinez.contato@gmail.com>
2024-04-19 18:09:42 -0300

    change env names

--- infra/database.js
+++ infra/database.js
@@ -3,12 +3,12 @@ const { Sequelize } = require("sequelize");
 console.log(process.env.DATABASE);
 
 const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DB_USER,
-  process.env.PASSWORD,
+  process.env.POSTGRES_DATABASE,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
   {
-    host: process.env.HOST,
-    port: process.env.DB_PORT,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     dialect: "postgres",
   },
 );
