7ddb1bfff366e967e227225c3fd7d0fa84d5dad3
rafael martinez <rafaelmartinez.contato@gmail.com>
2024-05-06 21:04:17 -0300

    use routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,18 @@ const express = require("express");
 const veterinarios = require("./veterinarioRoute.js");
 const tutores = require("./tutorRoute.js");
 const pets = require("./petRoute.js");
+const consultas = require("./consultaRoute.js");
+const exames = require("./exameRoute.js");
+const servicos = require("./servicoRoute.js");
 
 module.exports = (app) => {
-  app.use(express.json(), veterinarios, tutores, pets);
+  app.use(
+    express.json(),
+    veterinarios,
+    tutores,
+    pets,
+    consultas,
+    exames,
+    servicos,
+  );
 };
